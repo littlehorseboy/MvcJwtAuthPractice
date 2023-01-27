4424230292a1844290b50b65681f89ebff36f923
littlehorseboy <littlehorseboy@gmail.com>
2023-01-27 21:55:25 +0800

    feat: Token

---
+++ ApiControllers/TokenController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcJwtAuthPractice.Helpers;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcJwtAuthPractice.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly JwtHelpers _jwtHelpers;
+
+        public TokenController(JwtHelpers jwtHelpers)
+        {
+            this._jwtHelpers = jwtHelpers;
+        }
+
+        [HttpPost("signin")]
+        public ActionResult<string> Signin(LoginDto body)
+        {
+            if (body.Password != "haha")
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+
+            var token = _jwtHelpers.generateToken(body.UserName);
+            return Ok(new { token });
+        }
+
+        [Authorize]
+        [HttpGet("getClaims")]
+        public IActionResult GetClaims()
+        {
+            return Ok(User.Claims.Select(p => new { p.Type, p.Value }));
+        }
+
+        [Authorize]
+        [HttpGet("getUserName")]
+        public IActionResult GetUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+
+    }
+    public class LoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Helpers/JwtHelpers.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MvcJwtAuthPractice.Helpers
+{
+    public class JwtHelpers
+    {
+        private readonly IConfiguration Configuration;
+
+        public JwtHelpers(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public string generateToken(string userName)
+        {
+            var issuer = Configuration.GetValue<string>("JwtSettings:Issuer");
+            var signKey = Configuration.GetValue<string>("JwtSettings:SignKey");
+            var expireMinutes = Configuration.GetValue<string>("JwtSettings:ExpireMinutes");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Issuer = issuer,
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, userName),
+                    new Claim(ClaimTypes.Role, "Admin"),
+                    new Claim(ClaimTypes.Role, "User"),
+                    new Claim("carId", "horse"),
+                }),
+                Expires = DateTime.Now.AddMinutes(Convert.ToInt32(expireMinutes)),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signKey)),
+                    SecurityAlgorithms.HmacSha256Signature
+                ),
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            var serializeToken = tokenHandler.WriteToken(securityToken);
+
+            return serializeToken;
+        }
+    }
+}
